4001739a8938dec297c2bf03daef434bd5fb3e01
Yan Tarasov <vzu3427@gmail.com>
2025-12-02 15:44:20 +0200

    Added Tast 2

--- HW1_02_12/Program.cs
+++ HW1_02_12/Program.cs
@@ -1,33 +1,33 @@
-﻿namespace HW1_02_12;
+﻿//namespace HW1_02_12;
 
-internal class Program
-{
-    static void Main(string[] args)
-    {
-        int user_input;
+//internal class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        int user_input;
 
         
-        Console.Write("Please, enter number between 1-100: ");
-        user_input = Convert.ToInt32(Console.ReadLine());
+//        Console.Write("Please, enter number between 1-100: ");
+//        user_input = Convert.ToInt32(Console.ReadLine());
 
-        if (user_input < 1 || user_input > 100)
-        {
-            Console.WriteLine("Error try again");
-        }
+//        if (user_input < 1 || user_input > 100)
+//        {
+//            Console.WriteLine("Error try again");
+//        }
 
-        else
-        {
-            if (user_input % 3 == 0 && user_input % 5 == 0)
-                Console.WriteLine("Fizz Buzz");
+//        else
+//        {
+//            if (user_input % 3 == 0 && user_input % 5 == 0)
+//                Console.WriteLine("Fizz Buzz");
 
-            else if (user_input % 3 == 0)
-                Console.WriteLine("Fizz");
+//            else if (user_input % 3 == 0)
+//                Console.WriteLine("Fizz");
 
-            else if (user_input % 5 == 0)
-                Console.WriteLine("Buzz");
+//            else if (user_input % 5 == 0)
+//                Console.WriteLine("Buzz");
 
-            else
-                Console.WriteLine($"Number you input: {user_input}");
-        }
-    }
-}
+//            else
+//                Console.WriteLine($"Number you input: {user_input}");
+//        }
+//    }
+//}
---
+++ HW1_02_12/Task_2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1_02_12;
+
+internal class Task_2
+{
+    static void Main(string[] args)
+    {
+        float percentage = 0f;
+        int number = 0;
+
+        Console.Write("Enter number: ");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter percent: ");
+        percentage = Convert.ToSingle(Console.ReadLine());
+
+        if (number <= 0)
+        {
+            Console.WriteLine("Error: number must be grater than 0");
+        }
+
+        else
+        {
+            if (percentage < 0 || percentage > 100)
+            {
+                Console.WriteLine("Error: percentqge must be between 0 and 100");
+            }
+
+            else
+            {
+                float sum = (percentage * number) / 100;
+
+                Console.WriteLine($"{percentage} percentage from {number} = {sum}");
+            }
+                
+        }
+            
+    }
+}
