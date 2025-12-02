650356afac5797c10536d825260b5edbc3e24ac9
Yan Tarasov <vzu3427@gmail.com>
2025-12-02 16:26:05 +0200

    Added Task_3

--- HW1_02_12/Task_2.cs
+++ HW1_02_12/Task_2.cs
@@ -1,44 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace HW1_02_12;
+//namespace HW1_02_12;
 
-internal class Task_2
-{
-    static void Main(string[] args)
-    {
-        float percentage = 0f;
-        int number = 0;
+//internal class Task_2
+//{
+//    static void Main(string[] args)
+//    {
+//        float percentage = 0f;
+//        int number = 0;
 
-        Console.Write("Enter number: ");
-        number = Convert.ToInt32(Console.ReadLine());
+//        Console.Write("Enter number: ");
+//        number = Convert.ToInt32(Console.ReadLine());
 
-        Console.Write("Enter percent: ");
-        percentage = Convert.ToSingle(Console.ReadLine());
+//        Console.Write("Enter percent: ");
+//        percentage = Convert.ToSingle(Console.ReadLine());
 
-        if (number <= 0)
-        {
-            Console.WriteLine("Error: number must be grater than 0");
-        }
+//        if (number <= 0)
+//        {
+//            Console.WriteLine("Error: number must be grater than 0");
+//        }
 
-        else
-        {
-            if (percentage < 0 || percentage > 100)
-            {
-                Console.WriteLine("Error: percentqge must be between 0 and 100");
-            }
+//        else
+//        {
+//            if (percentage < 0 || percentage > 100)
+//            {
+//                Console.WriteLine("Error: percentqge must be between 0 and 100");
+//            }
 
-            else
-            {
-                float sum = (percentage * number) / 100;
+//            else
+//            {
+//                float sum = (percentage * number) / 100;
 
-                Console.WriteLine($"{percentage} percentage from {number} = {sum}");
-            }
+//                Console.WriteLine($"{percentage} percentage from {number} = {sum}");
+//            }
                 
-        }
+//        }
             
-    }
-}
+//    }
+//}
---
+++ HW1_02_12/Task_3.cs
@@ -0,0 +1,47 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace HW1_02_12;
+
+//internal class Task_3
+//{
+//    static void Main(string[] args)
+//    {
+//        int number;
+//        Console.Write("Enter number: ");
+//        number = Convert.ToInt32(Console.ReadLine());
+
+//        int digitsCount = 0;
+//        int temp = number;
+
+//        while (temp > 0)
+//        {
+//            digitsCount++;
+//            temp /= 10;
+//        }
+
+//        temp = number;
+//        int sum = 0;
+
+//        while (temp > 0)
+//        {
+//            int digit = temp % 10;
+//            sum += (int)Math.Pow(digit, digitsCount);
+//            temp /= 10;
+//        }
+
+//        if (sum == number)
+//        {
+//            Console.WriteLine($"{number} is an Armstrong number.");
+//        }
+
+//        else
+//        {
+//            Console.WriteLine($"{number} is not an Armstrong number.");
+//        }
+
+//    }
+//}
