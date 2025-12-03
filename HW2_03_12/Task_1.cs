3e15b382a67c239741bcc072112d241ef50f7a69
Yan Tarasov <vzu3427@gmail.com>
2025-12-03 21:07:21 +0200

    Added HW2

---
+++ HW2_03_12/Task_1.cs
@@ -0,0 +1,16 @@
+﻿namespace HW2_03_12;
+
+internal class Task_1
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter text: ");
+        string user_input = Console.ReadLine();
+
+        string[] words = user_input.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+        //StringSplitOptions.RemoveEmptyEntries делит строку по символу ' '
+
+        Console.WriteLine($"Word count: {words.Length}");
+    }
+}
