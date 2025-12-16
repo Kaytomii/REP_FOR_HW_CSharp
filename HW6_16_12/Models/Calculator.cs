11aaa678fd3aed1704fde00cd669c809ef7c431b
Yan Tarasov <vzu3427@gmail.com>
2025-12-16 21:09:32 +0200

    Added checked and unchecked

--- HW6_16_12/Models/Calculator.cs
+++ HW6_16_12/Models/Calculator.cs
@@ -1,60 +1,61 @@
 ﻿using System;
 
-namespace HW6_16_12.Models;
-
-public struct Calculator
+namespace HW6_16_12.Models
 {
-    public void Run()
+    public struct Calculator
     {
-        while (true)
+        public void Run()
         {
-            Console.WriteLine("  Calculator  ");
-            Console.WriteLine("0 - Exit");
-            Console.WriteLine("1 - Decimal -> Binary");
-            Console.WriteLine("2 - Binary -> Decimal");
-            Console.Write("Your choice: ");
-
-            if (!byte.TryParse(Console.ReadLine(), out byte choice))
+            while (true)
             {
-                Console.WriteLine("Invalid choice");
-                continue;
-            }
+                Console.WriteLine("    Calculator    ");
+                Console.WriteLine("0 - Exit");
+                Console.WriteLine("1 - Decimal -> Binary");
+                Console.WriteLine("2 - Binary -> Decimal");
+                Console.Write("Your choice: ");
 
-            if (choice == 0)
-            {
-                Console.WriteLine("Bye bye");
-                break;
-            }
-
-            Console.Write("Enter the number: ");
-            string? input = Console.ReadLine();
-
-            try
-            {
-                switch (choice)
+                if (!byte.TryParse(Console.ReadLine(), out byte choice))
                 {
-                    case 1:
-                        int decNumber = int.Parse(input);
-                        Console.WriteLine($"Decimal: {decNumber} Binary: {Convert.ToString(decNumber, 2)}");
-                        break;
-
-                    case 2:
-                        int binToDec = Convert.ToInt32(input, 2);
-                        Console.WriteLine($"Binary: {input} -> Decimal: {binToDec}");
-                        break;
-
-                    default:
-                        Console.WriteLine("Invalid menu option");
-                        break;
+                    Console.WriteLine("Invalid choice");
+                    continue;
                 }
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("Invalid number format");
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine("Number is out of range for type int");
+
+                if (choice == 0)
+                {
+                    Console.WriteLine("Bye bye");
+                    break;
+                }
+
+                Console.Write("Enter the number: ");
+                string? input = Console.ReadLine();
+
+                try
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                            int decNumber = checked(int.Parse(input));
+                            Console.WriteLine($"Decimal: {decNumber} Binary: {Convert.ToString(decNumber, 2)}");
+                            break;
+
+                        case 2:
+                            int binToDec = unchecked(Convert.ToInt32(input, 2));
+                            Console.WriteLine($"Binary: {input} -> Decimal: {binToDec}");
+                            break;
+
+                        default:
+                            Console.WriteLine("Invalid menu option");
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Invalid number format");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Number is out of range for type int");
+                }
             }
         }
     }
