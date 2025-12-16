ae1ca5ec0220c3a428f42f576c58f85cb7cc713f
Yan Tarasov <vzu3427@gmail.com>
2025-12-16 21:00:48 +0200

    Added HW6

---
+++ HW6_16_12/Models/Calculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace HW6_16_12.Models;
+
+public struct Calculator
+{
+    public void Run()
+    {
+        while (true)
+        {
+            Console.WriteLine("  Calculator  ");
+            Console.WriteLine("0 - Exit");
+            Console.WriteLine("1 - Decimal -> Binary");
+            Console.WriteLine("2 - Binary -> Decimal");
+            Console.Write("Your choice: ");
+
+            if (!byte.TryParse(Console.ReadLine(), out byte choice))
+            {
+                Console.WriteLine("Invalid choice");
+                continue;
+            }
+
+            if (choice == 0)
+            {
+                Console.WriteLine("Bye bye");
+                break;
+            }
+
+            Console.Write("Enter the number: ");
+            string? input = Console.ReadLine();
+
+            try
+            {
+                switch (choice)
+                {
+                    case 1:
+                        int decNumber = int.Parse(input);
+                        Console.WriteLine($"Decimal: {decNumber} Binary: {Convert.ToString(decNumber, 2)}");
+                        break;
+
+                    case 2:
+                        int binToDec = Convert.ToInt32(input, 2);
+                        Console.WriteLine($"Binary: {input} -> Decimal: {binToDec}");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid menu option");
+                        break;
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number format");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Number is out of range for type int");
+            }
+        }
+    }
+}
---
+++ HW6_16_12/Task_1.cs
@@ -0,0 +1,11 @@
+﻿namespace HW6_16_12;
+using Models;
+
+internal class Task_1
+{
+    static void Main(string[] args)
+    {
+       Calculator calculator = new Calculator();
+        calculator.Run();
+    }
+}
