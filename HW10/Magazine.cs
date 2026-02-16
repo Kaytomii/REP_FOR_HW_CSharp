65545fbe79ec9af1efb6e455d010c301f2e8dc37
Yan Tarasov <vzu3427@gmail.com>
2026-02-16 12:53:58 +0200

    Added HW10

---
+++ HW10/Magazine.cs
@@ -0,0 +1,26 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace HW10
+//{
+//   public class Magazine
+//    {
+//        public string Title { get; set; }
+//        public string Publisher { get; set; }
+//        public string ReleaseDate { get; set; }
+//        public int PageCount { get; set; }
+
+//        public void PrintInfo()
+//        {
+//            Console.WriteLine("Info about Magazine: ");
+//            Console.WriteLine($"Title: {Title}");
+//            Console.WriteLine($"Publisher: {Publisher}");
+//            Console.WriteLine($"Release date: {ReleaseDate}");
+//            Console.WriteLine($"Page count: {PageCount}");
+//        }
+
+//    }
+//}
---
+++ HW10/Program.cs
@@ -0,0 +1,68 @@
+﻿//using Newtonsoft.Json;
+//namespace HW10;
+
+//internal class Program
+//{
+//    static Magazine EnterMagazineInfo()
+//    {
+//        Magazine magazine = new Magazine();
+
+//        Console.Write("Enter magazine title: ");
+//        magazine.Title = Console.ReadLine();
+
+//        Console.Write("Enter publisher name: ");
+//        magazine.Publisher = Console.ReadLine();
+
+//        Console.Write("Enter release date (any format): ");
+//        magazine.ReleaseDate = Console.ReadLine();
+
+//        Console.Write("Enter page count: ");
+//        magazine.PageCount = int.Parse(Console.ReadLine());
+
+//        return magazine;
+//    }
+//    static string SerializeMagazine(Magazine magazine)
+//    {
+//        return JsonConvert.SerializeObject(magazine, Formatting.Indented);
+//    }
+
+//    static Magazine DeserializeMagazine(string json)
+//    {
+//        return JsonConvert.DeserializeObject<Magazine>(json);
+//    }
+
+//    static void SaveToFile(string json, string path)
+//    {
+//        File.WriteAllText(path, json);
+//    }
+
+//    static string LoadFromFile(string path)
+//    {
+//        return File.ReadAllText(path);
+//    }
+
+//    static void Main(string[] args)
+//    {
+//        Magazine magazine = EnterMagazineInfo();
+
+//        Console.WriteLine("\nDisplaying information:");
+//        magazine.PrintInfo();
+
+//        string json = SerializeMagazine(magazine);
+//        Console.WriteLine("\nSerialized JSON:");
+//        Console.WriteLine(json);
+
+//        string filePath = "magazine.json";
+//        SaveToFile(json, filePath);
+//        Console.WriteLine($"\nMagazine saved to file: {filePath}");
+
+//        Console.WriteLine("\nLoading magazine from file...");
+//        string loadedJson = LoadFromFile(filePath);
+
+//        Magazine deserializedMagazine = DeserializeMagazine(loadedJson);
+
+//        Console.WriteLine("\nDeserialized magazine:");
+//        deserializedMagazine.PrintInfo();
+
+//    }
+//}
